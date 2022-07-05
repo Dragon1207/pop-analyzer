291e29ff61c92713a8506fea1ea51169e38dfb1b
Dragon1207 <smileboth0805@outlook.com>
2022-07-06 02:22:30 +0900

    heroku

--- server/app.js
+++ server/app.js
@@ -17,7 +17,15 @@ const app = express();
 app.use(logger("dev"));
 app.use(json());
 app.use(urlencoded({ extended: false }));
-app.use(express.static(join(__dirname, "public")));
+const path = require('path')
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../client/public')))
+
+// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../client/public/index.html'))
+})
 
 // require api routes here after I create them
 app.use("/auth", require("./routes/auth"));
