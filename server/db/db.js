5f21680716808439aeddc0d17073ba01a4b12914
Dragon1207 <smileboth0805@outlook.com>
2022-07-06 03:06:43 +0900

    change db

--- server/db/db.js
+++ server/db/db.js
@@ -3,7 +3,7 @@ const Sequelize = require("sequelize");
 const db = new Sequelize(
   process.env.DATABASE_URL ||
     // "postgres://postgres:postgres@localhost:5432/population",
-    "postgres://ptctkbycavtxfx:ab2f1a942775c8b9d8d21962d9774a18015f810f8264d9cd681f000d9f2fef97@ec2-3-226-163-72.compute-1.amazonaws.com:5432/de589bt99vsmpp",
+    "postgres://jylevbqsmiaunr:7b72fe0846ae5053af5a6c8913ce3a614c3dcae7964460326f90dfdb68320b93@ec2-44-198-82-71.compute-1.amazonaws.com:5432/d8n7881o76u38f",
   {
     logging: false,
   }
