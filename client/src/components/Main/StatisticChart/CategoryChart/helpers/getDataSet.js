05b928f98783d7aad54a6dc1e037169e8bdb4a1b
Corey Breshears <coreybreshears619@gmail.com>
2022-07-05 08:36:23 -0400

    Config styles of each chart

---
+++ client/src/components/Main/StatisticChart/CategoryChart/helpers/getDataSet.js
@@ -0,0 +1,26 @@
+export default function (data) {
+  return {
+    labels: Object.keys(data),
+    datasets: [
+      {
+        data: Object.values(data),
+        backgroundColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        hoverBackgroundColor: [
+          "rgba(255, 99, 132, 0.5)",
+          "rgba(54, 162, 235, 0.5)",
+          "rgba(255, 206, 86, 0.5)",
+          "rgba(75, 192, 192, 0.5)",
+          "rgba(153, 102, 255, 0.5)",
+          "rgba(255, 159, 64, 0.5)",
+        ],
+      },
+    ],
+  };
+}
