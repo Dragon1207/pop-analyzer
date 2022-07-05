7a0b4472375911834db5bd98fc85dc24816c4f03
Dragon1207 <smileboth0805@outlook.com>
2022-07-06 02:04:33 +0900

    convert

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,8 @@ const Sequelize = require("sequelize");
 
 const db = new Sequelize(
   process.env.DATABASE_URL ||
-    "postgres://corey:password@localhost:5432/population",
+    // "postgres://postgres:postgres@localhost:5432/population",
+    "postgres://sxxstekoovqtur:c55e52d09cdd3dd444945b53194a5afc7070c9c6b5d1a757ce15287752ce0aaa@ec2-34-233-115-14.compute-1.amazonaws.com:5432/ddpi80mhfed5ec",
   {
     logging: false,
   }
