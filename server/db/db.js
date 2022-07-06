ee4710da82370835a9d3b487db200a4973be0508
Dragon1207 <smileboth0805@outlook.com>
2022-07-06 09:07:35 +0900

    dbdbdb

--- server/db/db.js
+++ server/db/db.js
@@ -4,8 +4,14 @@ const db = new Sequelize(
   process.env.DATABASE_URL ||
     // "postgres://postgres:postgres@localhost:5432/population",
     "postgres://ggasnuadbwhkrp:bf430ef220df4299010645b8984c0721373de79bc5d379b6b99e1d277faae634@ec2-34-225-159-178.compute-1.amazonaws.com:5432/dbm88fkif79epo",
+    
   {
     logging: false,
+    dialectOptions: {
+      ssl: {
+          rejectUnauthorized: false
+      }
+    }
   }
 );
 
